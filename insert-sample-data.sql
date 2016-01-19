f363a76633a9b870494e30c665d8f1266b9e5099
ogorkovets <ogorkovets@mail.greenriver.edu>
2016-01-18 19:41:26 -0800

    My first version of the site

---
+++ insert-sample-data.sql
@@ -0,0 +1,15 @@
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (1, 'magna', '2015-09-25', 39, 58, '2015-12-23 21:27:33');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (2, 'consectetuer', '2015-05-03', 92, 48, '2015-03-13 17:04:45');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (3, 'nisl', '2015-06-12', 60, 59, '2015-05-14 05:40:34');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (4, 'ut', '2015-04-20', 86, 95, '2015-02-17 08:57:29');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (5, 'vel', '2015-09-22', 2, 100, '2015-09-17 22:51:26');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (6, 'natoque', '2015-04-11', 26, 66, '2015-07-10 02:06:29');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (7, 'aliquet', '2015-03-22', 55, 7, '2015-12-03 07:33:47');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (8, 'condimentum', '2015-10-10', 31, 11, '2015-07-27 14:33:46');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (9, 'vitae', '2015-07-13', 84, 36, '2015-11-25 05:44:31');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (10, 'nibh', '2015-09-28', 45, 15, '2015-12-28 11:05:10');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (11, 'duis', '2015-09-22', 15, 22, '2015-11-22 01:00:29');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (12, 'consectetuer', '2015-11-16', 82, 55, '2015-05-05 10:33:12');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (13, 'odio', '2015-08-26', 51, 54, '2015-02-10 16:12:24');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (14, 'convallis', '2015-07-05', 17, 83, '2015-10-24 04:43:23');
+insert into observation  (observation_id, hive_name, observation_date, duration, mite_count, submit_date) values (15, 'in', '2015-11-04', 63, 42, '2015-09-23 12:39:15');
